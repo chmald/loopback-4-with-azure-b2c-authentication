e98572d9373f3b96ce082c1b6ef9ad3fc5ee9836
Christopher Maldonado <chmald@microsoft.com>
2020-09-09 17:41:07 -0500

    removing dist folder

--- dist/__tests__/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { Chmaldloopback4Application } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: Chmaldloopback4Application;
-    client: Client;
-}
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Chmaldloopback4Application } from './application';
-import { ApplicationConfig } from '@loopback/core';
-export { Chmaldloopback4Application };
-export declare function main(options?: ApplicationConfig): Promise<Chmaldloopback4Application>;
